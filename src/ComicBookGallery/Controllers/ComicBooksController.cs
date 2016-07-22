118e9c2b2050465ee3d3c66ad1947c488a24c391
wen <wen@DESKTOP-LDAV1O9>
2016-07-22 17:25:12 -0500

    add detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hellp from the comic books controller!";
+        }
     }
 }
