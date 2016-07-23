d4bc8b52d4481bb797ba8433b7b4b466dd439ad8
wen <wen@DESKTOP-LDAV1O9>
2016-07-22 20:07:59 -0500

    updated comic book controller to use respository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,21 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan SlottScript", Role = "Script" },
-                    new Artist() { Name = "Humberto RamosPencils", Role = "Pencils" },
-                    new Artist() { Name = "Victor OlazabaInks", Role = "Inks" },
-                    new Artist() { Name = "Edgar DelgadoColors", Role = "Colors" },
-                    new Artist() { Name = "Chris EliopoulosLetters", Role = "Letters" },
-                }
-            };
+        private ComicBookRespository _comicBookRepository = null;
 
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRespository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);
         }
     }
